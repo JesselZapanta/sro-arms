c72d94f88abf5664a4b09ea49c2b0747c76f35f6
JesselZapanta <jesselzapanta9@gmail.com>
2025-04-05 14:44:08 +0800

    login

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -2,16 +2,34 @@ import InputError from "@/Components/InputError";
 import { Button } from "@/Components/ui/button";
 import { Input } from "@/Components/ui/input";
 import { Label } from "@/Components/ui/label";
+import { router } from "@inertiajs/react";
+import { Loader2 } from "lucide-react";
 import { useState } from "react";
 
 export default function Login() {
-    
+
     const [formData, setFormData] = useState({
-        email: "",
-        password: "",
-    });
-    const [errors, setErrors] = useState({});
-    const [processing, setProcessing] = useState(false);
+            studentId: "",
+            password: "",
+        });
+        const [errors, setErrors] = useState({});
+        const [processing, setProcessing] = useState(false);
+    
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            setProcessing(true);
+    
+            try {
+                const res = await axios.post("/login", formData);
+                if (res.data.status === "login") {
+                    router.visit("/login"); //change to dashboard controller
+                }
+            } catch (err) {
+                setErrors(err.response.data.errors);
+            } finally {
+                setProcessing(false);
+            }
+        };
 
     return (
         <div className="grid min-h-svh lg:grid-cols-2">
@@ -38,25 +56,33 @@ export default function Login() {
                 </div>
                 <div className="flex flex-1 items-center justify-center">
                     <div className="w-full max-w-sm rounded-lg border bg-white p-6 shadow-md">
-                        <form className="flex flex-col gap-6">
+                        <form onSubmit={handleSubmit} className="flex flex-col gap-6">
                             <div className="flex flex-col items-center gap-2 text-center">
                                 <h1 className="text-2xl font-bold">
                                     Login to your account
                                 </h1>
                                 <p className="text-balance text-sm text-muted-foreground">
-                                    Enter your email below to login to your
+                                    Enter your student ID below to login to your
                                     account
                                 </p>
                             </div>
                             <div className="grid gap-6">
                                 <div className="grid gap-2">
-                                    <Label htmlFor="email">Email</Label>
+                                    <Label htmlFor="studentId">
+                                        Student ID
+                                    </Label>
                                     <Input
-                                        id="email"
-                                        type="email"
-                                        placeholder="m@example.com"
+                                        id="studentId"
+                                        type="number"
+                                        value={formData.studentId}
+                                        onChange={(e) => {
+                                            setFormData({
+                                                ...formData,
+                                                studentId: e.target.value,
+                                            });
+                                        }}
                                     />
-                                    <InputError message={errors.email} />
+                                    <InputError message={errors.studentId} />
                                 </div>
                                 <div className="grid gap-2">
                                     <div className="flex items-center">
@@ -70,11 +96,32 @@ export default function Login() {
                                             Forgot your password?
                                         </a> */}
                                     </div>
-                                    <Input id="password" type="password" />
+                                    <Input
+                                        id="password"
+                                        type="password"
+                                        value={formData.password}
+                                        onChange={(e) => {
+                                            setFormData({
+                                                ...formData,
+                                                password: e.target.value,
+                                            });
+                                        }}
+                                    />
                                     <InputError message={errors.password} />
                                 </div>
-                                <Button type="submit" className="w-full">
-                                    Login
+                                <Button
+                                    type="submit"
+                                    disabled={processing}
+                                    className="w-full"
+                                >
+                                    {processing ? (
+                                        <span className="flex items-center gap-2">
+                                            <Loader2 className="animate-spin" />
+                                            Please wait
+                                        </span>
+                                    ) : (
+                                        "Log In"
+                                    )}
                                 </Button>
                             </div>
                             <div className="text-center text-sm">
