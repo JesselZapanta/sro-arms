301f58b8ed5d807d3e66df9cba22011eabea7262
JesselZapanta <jesselzapanta9@gmail.com>
2025-04-11 21:04:18 +0800

    user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -25,7 +25,31 @@ class UserSeeder extends Seeder
                 'email' => 'arms@gmail.com',
                 'password' => Hash::make('arms'),
                 'role' => 1, // 1: admin
-                'status' => 1, // 1: admin
+                'status' => 1, // 1: active
+            ],
+            [
+                'firstname' => 'ARMS',
+                'middlename' => null,
+                'lastname' => 'Student',
+                'institute' => 'ICS',
+                'organization' => 'JPEG.COM',
+                'studentId' => '123',
+                'email' => 'student@gmail.com',
+                'password' => Hash::make('student'),
+                'role' => 2, // 1: Student
+                'status' => 1, // 1: active
+            ],
+            [
+                'firstname' => 'ARMS',
+                'middlename' => null,
+                'lastname' => 'Officer',
+                'institute' => 'SRO Federated',
+                'organization' => 'SRO Federated',
+                'studentId' => '321',
+                'email' => 'officer@gmail.com',
+                'password' => Hash::make('officer'),
+                'role' => 2, // 1: Officer
+                'status' => 1, // 1: active
             ],
         ];
 
